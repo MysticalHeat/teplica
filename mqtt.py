43cee77cdc1df8485d379cdf4a51f22c278f0bf0
Artyom <appuzerenko@mail.ru>
2021-10-22 23:34:52 +0300

    типо прием данных от сервака

--- mqtt.py
+++ mqtt.py
@@ -1,1 +1,24 @@
-######test
+import paho.mqtt.client as mqtt
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with the result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+
+def on_message(client, userdata, msg):
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
+    print(msg.topic+" "+str(msg.payload))
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("mqtt.eclipseprojects.io", 1883, 60)
+client.loop_forever()
