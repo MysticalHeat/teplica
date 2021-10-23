6045cb02fa64605dbb0892425cfc5db879990a23
MysticalHeat <ilyaelmanov@gmail.com>
2021-10-23 03:33:23 +0300

    добавлен коннект к тестовому mqtt-брокеру

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, message):
-    print "Message received: " + message.payload
+    print("Message received:", message.payload.decode('UTF-8'))
 
 
 Connected = False  # global variable for the state of the connection
 
-broker = 'mqtt0.bast-dev.ru'
+broker = 'test.mosquitto.org'
 port = 1883
 topic_prefix = "service/weather_logger"
 username = 'hackathon'
@@ -29,7 +29,7 @@ password = 'Autumn2021'
 
 
 client = mqttClient.Client("Python")  # create new instance
-client.username_pw_set(username, password=password)  # set username and password
+# client.username_pw_set(username, password=password)  # set username and password
 client.on_connect = on_connect  # attach function to callback
 client.on_message = on_message  # attach function to callback
 
@@ -40,13 +40,13 @@ client.loop_start()  # start the loop
 while Connected != True:  # Wait for connection
     time.sleep(0.1)
 
-client.subscribe("python/test")
+client.subscribe(topic_prefix)
 
 try:
     while True:
         time.sleep(1)
 
 except KeyboardInterrupt:
-    print "exiting"
+    print("exiting")
     client.disconnect()
     client.loop_stop()
