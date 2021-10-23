22fa0dd8003b0885745a32dec24f7af5a92aed73
MysticalHeat <ilyaelmanov@gmail.com>
2021-10-23 11:04:57 +0300

    реализована запись данных с датчиков в бд

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import paho.mqtt.client as mqttClient
 import time
+import psycopg2
+import datetime
+from psycopg2 import Error
+
+
+def export_info(sensor_info):
+    try:
+        conn = psycopg2.connect(dbname='postgres', user='postgres', host='localhost', password='admin')
+        cur = conn.cursor()
+        cur.execute(
+            """INSERT INTO sensor_info (air_temp, ground_temp, air_humidity, soil_humidity, illumination, date, time)
+            VALUES (%s, %s, %s, %s, %s, %s, %s);
+            """,
+            sensor_info
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+        print("Соединение с PostgreSQL закрыто")
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
 
 
 def on_connect(client, userdata, flags, rc):
@@ -14,9 +35,16 @@ def on_connect(client, userdata, flags, rc):
 
         print("Connection failed")
 
+sensors_info = []
 
 def on_message(client, userdata, message):
-    print("Message received:", message.payload.decode('UTF-8'))
+    now = datetime.datetime.now()
+    inputdata = message.payload.decode('UTF-8')
+    sensors_info = [*inputdata.split('\n'), now.strftime('%d/%m/%Y'), now.strftime('%H:%M:%S')]
+    for i in range(5):
+        _, sensors_info[i] = sensors_info[i].split(':')
+        sensors_info[i] = int(sensors_info[i])
+    export_info(sensors_info)
 
 
 Connected = False  # global variable for the state of the connection
